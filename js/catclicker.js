6c7cc0e6f19ef5669b7132f20db8fbf0a980eeed
Aaron Justman <AaronJ@Aarons-MBP.home>
2015-03-04 22:25:49 -0800

    added second cat to cat clicker duel.

--- js/catclicker.js
+++ js/catclicker.js
@@ -1,14 +1,44 @@
 
-// console.log( "this is cat clicker!!!" );
+// counters for each cat
+var cat1ClickCount = 0, cat2ClickCount = 0;
 
-var clickCount = 0;
+// names for each cat
+var cat1Name = "Garfield", cat2Name = "Heathcliff";
 
-var catPic = document.getElementById( "cat-pic" );
-var clickText = document.getElementById( "num-clicks" );
+// get the image associated with each cat
+var cat1Pic = document.getElementById( "cat-pic-1" );
+var cat2Pic = document.getElementById( "cat-pic-2" );
 
-catPic.addEventListener( "click", function()
+// get the spans where each cat's name appears
+var cat1NameSpans = document.getElementsByClassName( "cat-1-name" );
+var cat2NameSpans = document.getElementsByClassName( "cat-2-name" );
+
+// assign all instances of each cat's name
+for( var i = 0; i < cat1NameSpans.length; i++ )
+{
+	cat1NameSpans[ i ].innerHTML = cat1Name;
+}
+
+for( var j = 0; j < cat2NameSpans.length; j++ )
+{
+	cat2NameSpans[ j ].innerHTML = cat2Name;
+}
+
+// get the span where the number of clicks is updated
+var clickText1 = document.getElementById( "cat-1-clicks" );
+var clickText2 = document.getElementById( "cat-2-clicks" );
+
+// assign event listeners to each image
+cat1Pic.addEventListener( "click", function()
 	{
-		clickText.innerHTML = ++clickCount;
+		clickText1.innerHTML = ++cat1ClickCount;
+	},
+	false
+);
+
+cat2Pic.addEventListener( "click", function()
+	{
+		clickText2.innerHTML = ++cat2ClickCount;
 	},
 	false
 );
--- js/catclickerjQuery.js
+++ js/catclickerjQuery.js
@@ -1,10 +1,39 @@
 
-var clickCount = 0;
+// counters for each cat
+var cat1ClickCount = 0, cat2ClickCount = 0;
 
-var clickText = $( "#num-clicks" );
+// names for each cat
+var cat1Name = "Garfield", cat2Name = "Heathcliff";
 
-$( "#cat-pic" ).click( function( e )
+// get the spans where each cat's name appears
+var cat1NameSpans = $( ".cat-1-name" );
+var cat2NameSpans = $( ".cat-2-name" );
+console.log( cat1NameSpans );
+
+// assign all instances of each cat's name
+for( var i = 0; i < cat1NameSpans.length; i++ )
+{
+	cat1NameSpans[ i ].innerHTML = cat1Name;
+}
+
+for( var j = 0; j < cat2NameSpans.length; j++ )
+{
+	cat2NameSpans[ j ].innerHTML = cat2Name;
+}
+
+// get the span where the number of clicks is updated
+var clickText1 = $( "#cat-1-clicks" );
+var clickText2 = $( "#cat-2-clicks" );
+
+// assign event listeners to each image
+$( "#cat-pic-1" ).click( function( e )
 	{
-		clickText.text( ++clickCount );
+		clickText1.text( ++cat1ClickCount );
+	}
+);
+
+$( "#cat-pic-2" ).click( function( e )
+	{
+		clickText2.text( ++cat2ClickCount );
 	}
 );
