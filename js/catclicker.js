f15ed84626aa9718d5846e38ad3deabd2dc2f60d
Aaron Justman <AaronJ@Aarons-MBP.home>
2015-03-05 08:45:44 -0800

    Now supports 5 cats. Now uses Bootstrap. Updated for Vanilla JS and for jQuery.

--- js/catclicker.js
+++ js/catclicker.js
@@ -1,36 +1,53 @@
 
 // counters for each cat
-var cat1ClickCount = 0, cat2ClickCount = 0;
+var cat1ClickCount = 0,
+	cat2ClickCount = 0,
+	cat3ClickCount = 0,
+	cat4ClickCount = 0,
+	cat5ClickCount = 0;
 
 // names for each cat
-var cat1Name = "Garfield", cat2Name = "Heathcliff";
+var catNames =
+[
+	"Garfield",
+	"Heathcliff",
+	"Lorraine",
+	"Tigger",
+	"Hobbes"
+];
 
 // get the image associated with each cat
-var cat1Pic = document.getElementById( "cat-pic-1" );
-var cat2Pic = document.getElementById( "cat-pic-2" );
+var cat1Pic = document.getElementById( "cat-1-pic" );
+var cat2Pic = document.getElementById( "cat-2-pic" );
+var cat3Pic = document.getElementById( "cat-3-pic" );
+var cat4Pic = document.getElementById( "cat-4-pic" );
+var cat5Pic = document.getElementById( "cat-5-pic" );
+var catDisplayPic = document.getElementById( "cat-display-pic" );
 
 // get the spans where each cat's name appears
-var cat1NameSpans = document.getElementsByClassName( "cat-1-name" );
-var cat2NameSpans = document.getElementsByClassName( "cat-2-name" );
+var catNameHeaders = document.getElementsByClassName( "cat-name-header" );
+var catNameMains = document.getElementsByClassName( "cat-name-main" );
 
 // assign all instances of each cat's name
-for( var i = 0; i < cat1NameSpans.length; i++ )
+for( var i = 0; i < catNames.length; i++ )
 {
-	cat1NameSpans[ i ].innerHTML = cat1Name;
-}
-
-for( var j = 0; j < cat2NameSpans.length; j++ )
-{
-	cat2NameSpans[ j ].innerHTML = cat2Name;
+	catNameHeaders[ i ].innerHTML = catNames[ i ];
+	catNameMains[ i ].innerHTML = catNames[ i ];
 }
 
 // get the span where the number of clicks is updated
 var clickText1 = document.getElementById( "cat-1-clicks" );
 var clickText2 = document.getElementById( "cat-2-clicks" );
+var clickText3 = document.getElementById( "cat-3-clicks" );
+var clickText4 = document.getElementById( "cat-4-clicks" );
+var clickText5 = document.getElementById( "cat-5-clicks" );
 
 // assign event listeners to each image
+// when the thumbnail is clicked, the counter increases,
+// and the main display will show a big pic of the cliced cat
 cat1Pic.addEventListener( "click", function()
 	{
+		catDisplayPic.src = cat1Pic.src;
 		clickText1.innerHTML = ++cat1ClickCount;
 	},
 	false
@@ -38,7 +55,32 @@ cat1Pic.addEventListener( "click", function()
 
 cat2Pic.addEventListener( "click", function()
 	{
+		catDisplayPic.src = cat2Pic.src;
 		clickText2.innerHTML = ++cat2ClickCount;
+	},
+	false
+);
+
+cat3Pic.addEventListener( "click", function()
+	{
+		catDisplayPic.src = cat3Pic.src;
+		clickText3.innerHTML = ++cat3ClickCount;
+	},
+	false
+);
+
+cat4Pic.addEventListener( "click", function()
+	{
+		catDisplayPic.src = cat4Pic.src;
+		clickText4.innerHTML = ++cat4ClickCount;
+	},
+	false
+);
+
+cat5Pic.addEventListener( "click", function()
+	{
+		catDisplayPic.src = cat5Pic.src;
+		clickText5.innerHTML = ++cat5ClickCount;
 	},
 	false
 );
--- js/catclickerjQuery.js
+++ js/catclickerjQuery.js
@@ -1,39 +1,81 @@
 
 // counters for each cat
-var cat1ClickCount = 0, cat2ClickCount = 0;
+var cat1ClickCount = 0,
+	cat2ClickCount = 0,
+	cat3ClickCount = 0,
+	cat4ClickCount = 0,
+	cat5ClickCount = 0;
 
 // names for each cat
-var cat1Name = "Garfield", cat2Name = "Heathcliff";
+var catNames =
+[
+	"Garfield",
+	"Heathcliff",
+	"Lorraine",
+	"Tigger",
+	"Hobbes"
+];
+
+// get the image associated with each cat
+var cat1Pic = $( "#cat-1-pic" );
+var cat2Pic = $( "#cat-2-pic" );
+var cat3Pic = $( "#cat-3-pic" );
+var cat4Pic = $( "#cat-4-pic" );
+var cat5Pic = $( "#cat-5-pic" );
+var catDisplayPic = $( "#cat-display-pic" );
 
 // get the spans where each cat's name appears
-var cat1NameSpans = $( ".cat-1-name" );
-var cat2NameSpans = $( ".cat-2-name" );
-console.log( cat1NameSpans );
+var catNameHeaders = $( ".cat-name-header" );
+var catNameMains = $( ".cat-name-main" );
 
 // assign all instances of each cat's name
-for( var i = 0; i < cat1NameSpans.length; i++ )
+for( var i = 0; i < catNames.length; i++ )
 {
-	cat1NameSpans[ i ].innerHTML = cat1Name;
-}
-
-for( var j = 0; j < cat2NameSpans.length; j++ )
-{
-	cat2NameSpans[ j ].innerHTML = cat2Name;
+	catNameHeaders[ i ].innerHTML = catNames[ i ];
+	catNameMains[ i ].innerHTML = catNames[ i ];
 }
 
 // get the span where the number of clicks is updated
 var clickText1 = $( "#cat-1-clicks" );
 var clickText2 = $( "#cat-2-clicks" );
+var clickText3 = $( "#cat-3-clicks" );
+var clickText4 = $( "#cat-4-clicks" );
+var clickText5 = $( "#cat-5-clicks" );
 
 // assign event listeners to each image
-$( "#cat-pic-1" ).click( function( e )
+// when the thumbnail is clicked, the counter increases,
+// and the main display will show a big pic of the cliced cat
+$( "#cat-1-pic" ).click( function( e )
 	{
+		catDisplayPic.attr( "src", cat1Pic.attr( "src" ) );
 		clickText1.text( ++cat1ClickCount );
 	}
 );
 
-$( "#cat-pic-2" ).click( function( e )
+$( "#cat-2-pic" ).click( function( e )
 	{
+		catDisplayPic.attr( "src", cat2Pic.attr( "src" ) );
 		clickText2.text( ++cat2ClickCount );
+	}
+);
+
+$( "#cat-3-pic" ).click( function( e )
+	{
+		catDisplayPic.attr( "src", cat3Pic.attr( "src" ) );
+		clickText3.text( ++cat3ClickCount );
+	}
+);
+
+$( "#cat-4-pic" ).click( function( e )
+	{
+		catDisplayPic.attr( "src", cat4Pic.attr( "src" ) );
+		clickText4.text( ++cat4ClickCount );
+	}
+);
+
+$( "#cat-5-pic" ).click( function( e )
+	{
+		catDisplayPic.attr( "src", cat5Pic.attr( "src" ) );
+		clickText5.text( ++cat5ClickCount );
 	}
 );
